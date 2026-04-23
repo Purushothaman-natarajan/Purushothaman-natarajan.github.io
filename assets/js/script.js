a40175280c5ca3dae360fbf31600a5579b5ee7e8
Purushothaman Natarajan <purushothamanprt@gmail.com>
2026-04-23 11:17:11 +0530

    Fix: mobile alignment, preloader delay, menu button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('load', () => {
       setTimeout(() => {
         preloader.classList.add('loaded');
-      }, 600);
+      }, 1200);
     });
   }
 
